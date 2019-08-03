62c46325a02b82fcad9fb78e4b06719b9422272d
Caranell <lyachek.anton@gmail.com>
2019-08-03 22:06:04 +0500

    Updates

---
+++ precache-manifest.8b16452e899c1e5e1e1ed654c06ddc56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2120c9b95dab7c310b8741071dfd1ba",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "bbaf6a86706e6260b355",
+    "url": "/react-todo/static/css/main.4bb0d698.chunk.css"
+  },
+  {
+    "revision": "0a8bb30db8b49bd0664d",
+    "url": "/react-todo/static/js/2.7e630792.chunk.js"
+  },
+  {
+    "revision": "bbaf6a86706e6260b355",
+    "url": "/react-todo/static/js/main.a103d056.chunk.js"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  }
+]);
